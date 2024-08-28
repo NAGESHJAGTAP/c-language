5fcc9a29d330f013864091c39f68409162081381
PRIY MAVANI <priy.mavani.cg@gmail.com>
2024-08-28 21:59:26 +0530

    new

---
+++ basic.c
@@ -0,0 +1,109 @@
+
+#include <stdio.h>
+
+int main() {
+    // 1
+    // printf("HELLO, WORLD!");
+
+    // return 0;
+
+// 2
+//    int a= 3;
+//    int b = 6;
+//    printf("%d",a+b);
+
+// 3
+// int a = 2;
+// int b = 8;
+// int c = 5;
+
+
+
+// if(a >b && a >c){
+//     printf("%d",a);
+// }
+// else  if(c >b && c >a){
+//     printf("%d",c);
+// }else{
+//     printf("%d",b);
+// }
+// return 0;
+
+//4
+
+// int a = 4;
+// if(a%2 == 0){
+//     printf("%d prime ",a);
+// }else{
+//     printf("%d  not prime",a);
+// }
+
+//5
+// int count = 1;
+// int num = 6;
+// for(int i =1 ; i< =num; i++ ){
+//     count *= i;
+//     // printf("%d\n",count);
+// }
+// printf("%d\n",count);
+// // 8
+//     char array[] = {2,4,9,8};
+//     int min = array[0] ;
+//     for(int i = 0 ; i<strlen(array);i++){
+//         if(min>array[i]){
+//             min = array[i];
+//         }
+// }
+// printf("%d\n",min);
+// }
+
+// // 11
+// char array[]  = {1,2,3,4,5};
+// int count = 0;
+// for(int i =0 ; i<strlen(array);i++){
+//     count += i;
+//   // printf("%d\n",count);
+// }
+// printf("%d\n",count);
+
+// // 13
+// int num = 2;
+// int count = 0;
+// for (int i =2 ; i<num;i++){
+//     if(num % i == 0){
+//         count += 1;
+//         // printf("%d\n",count);
+//     }
+// }
+//     if(count == 0){
+//         printf("%d\nprime",num);
+        
+//     }else{
+//         printf("%d\nnot prime ",num);
+//     }
+
+// 14
+// char str[]="hello";
+// int count = 0;
+// for(int i = 0;i<strlen(str);i++){
+//     if(str[i] == 'a'||str[i] == 'e'||str[i] == 'i'||str[i] == 'o'||str[i] == 'u'){
+//         count += 1;
+        
+//     }
+// }
+// printf("%d",count);
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
