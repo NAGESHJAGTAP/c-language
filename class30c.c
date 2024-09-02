8d7446d7cf34308c31a44a340334838153b60d7e
PRIY MAVANI <priy.mavani.cg@gmail.com>
2024-09-02 09:40:04 +0530

    aug

---
+++ class30c.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+int main() {
+    
+//int num;
+//scanf(" %d",&num);
+//// printf("%d\n",num);
+//printf(" value of num %d",num);
+//
+//float marks;
+//scanf("%f",&marks);
+//if(marks>=15){
+//	printf("PASS");}
+//	else {
+//		printf("FAIL");60
+//	}
+
+//float age;
+//scanf(" enter age %f",&age);
+//if(age > 60){
+//	printf("senior citizen \n");
+//	
+//}else if(age > 18 && age <=60){
+//	printf("young\n");
+//
+//	
+//}else{
+//	printf("baache ho tum kelo kudo moj karo \n");
+//}
+//
+//return 0;
+
+//    float age;
+//    printf("Enter age: ");
+//    scanf("%f", &age);
+//
+//    if (age > 60) {
+//        printf("Senior citizen\n");
+//    } else if (age > 18 && age <= 60) {
+//        printf("Young\n");
+//    } else {
+//        printf("Bacche ho tum, khelo kudo, moj karo\n");
+//    }
+
+
+float mark;
+printf("ENTER MARKS : ");
+scanf("%f",&mark);
+if(mark>=25){
+	printf("A");
+}else if (mark>20 && mark <25){
+	printf("B");
+}else if (mark>15 && mark <20){
+	printf("c");
+}else{
+	printf("d");
+}
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+
